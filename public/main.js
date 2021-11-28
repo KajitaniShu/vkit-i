5080cc74613e57c37560def2f71167e4124ea5e2
kajitani <p1u2r0i7nn@yahoo.co.jp>
2021-11-29 00:47:02 +0900

    ボタンの位置を真ん中に変更

--- public/main.js
+++ public/main.js
@@ -15,8 +15,7 @@ socket.on('connect', async () =>{
     $('body').css('background-color','white');
     $('#canvas1').show();
     $('#canvas2').show();
-    //$("#main").append('<h1 class="main-title">バーチャル九工大情報工学部</h1>')
-    $("#main").append('<button id="start-button" class="start-button">start</button>')
+    $(".display-wrap").append('<button id="start-button">start</button>')
     $("#start-button").click(function(){
         $(this).remove();
         world.Start();
