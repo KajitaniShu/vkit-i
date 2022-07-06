2a9d907fd29f30a288acd12f5e50b409a563bc3c
kajitanipc <kajitanishu@gmail.com~>
2022-07-06 13:59:23 +0900

    動画の配置を調整

--- src/containers/Youtube.tsx
+++ src/containers/Youtube.tsx
@@ -24,15 +24,17 @@ const Youtube = ({ modelPath}) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     return (
         <>
-        <mesh scale={[0.19, 0.19, 0.19]} position={[2, 0, 0.5]}>
-            <primitive
-            // @ts-ignore
-            object={gltf.scene}
-            />
+        <mesh scale={[1.2, 1.2, 1.2]} position={[2, 0, 0.5]}>
+            <mesh scale={[0.19, 0.19, 0.19]} position={[0, 0, 0]}>
+                <primitive
+                // @ts-ignore
+                object={gltf.scene}
+                />
+            </mesh>
+            <Html position={[0, 1.05, 0.02]} transform occlude distanceFactor={1.1} center={true} >
+                <iframe width="720" height="420" src="https://www.youtube.com/embed/videoseries?autoplay=1&loop=1&mute=1?controls=0&amp;list=PLW67WEKYaxqNXDn51SIQj49ZlHFYKKIjE" title="研究紹介" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+            </Html>
         </mesh>
-        <Html position={[2, 1.05, 0.52]} transform occlude distanceFactor={1.1} center={true} >
-            <iframe width="720" height="420" src="https://www.youtube.com/embed/videoseries?autoplay=1&loop=1&mute=1?controls=0&amp;list=PLW67WEKYaxqNXDn51SIQj49ZlHFYKKIjE" title="研究紹介" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-        </Html>
         </>
     );
 }
