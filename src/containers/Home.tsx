eeafe1381658154e4bed3d7d087461867f73495e
kajitanipc <kajitanishu@gmail.com~>
2023-11-03 22:48:25 +0900

    Fix スマホ版でメニューバーが押せない問題を解消

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -24,7 +24,7 @@ export function Home() {
   return (
     <>
     <div style={{position: "relative", height: "100vh"}}>
-      <Box sx={{position: "absolute"}} style={{zIndex: width > 700 ? -1 : 100}}  bg="transparent" h="100vh" w="100%" pt="0vh">
+      <Box sx={{position: "absolute"}} style={{zIndex: width > 700 ? -1 : 3}}  bg="transparent" h="100vh" w="100%" pt="0vh">
         <Container size="md" px="xl" mt="60vh" >
           <Title sx={{position: "absolute", zIndex:100}}>九工大</Title>
           <Title sx={{position: "absolute", zIndex:100}} mt={rem(45)}>バーチャルキャンパス</Title>
@@ -55,18 +55,18 @@ export function Home() {
         />
         <color attach="background" args={['#ECEEF1']} />
         <group position={[10, 0, 10]}>
-          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={1000} AreaHeight={100000000} AreaWidth={10000000} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
+          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={10} AreaHeight={10} AreaWidth={10} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
         </group>
         <group position={[-10, 0, 10]}>
-          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={1000} AreaHeight={100000000} AreaWidth={10000000} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
+          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={10} AreaHeight={10} AreaWidth={10} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
         </group>
         <group position={[10, 0, -10]}>
-          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={1000} AreaHeight={100000000} AreaWidth={10000000} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
+          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={10} AreaHeight={10} AreaWidth={10} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
         </group>
         <group position={[-10, 0, -10]}>
-          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={1000} AreaHeight={100000000} AreaWidth={10000000} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
+          <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={10} AreaHeight={10} AreaWidth={10} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
         </group>
-        <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={1000} AreaHeight={100000000} AreaWidth={10000000} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
+        <ExplosionConfetti  rate={0.4} amount={40} fallingHeight={6} fallingSpeed={0.01} Radius={10} AreaHeight={10} AreaWidth={10} enableShadows={false} isExploding colors={['yellow', 'white', 'red', 'blue', '#6BCB77']}/>
 
         <Grid cellColor="#ddd" cellThickness={0.7} sectionThickness={0.7} sectionColor="#fff"  fadeStrength={1} position={[0, -0.04, 0]} args={[500, 500]} />
         <SoftShadows />
@@ -75,7 +75,7 @@ export function Home() {
 
         <HomeScene modelPath={"./kyutech_map_2.glb"}/>
         <VideoBoard video_path={path.board.video_path} position={[-5.689, 1.174, 13.55]}/>
-        <OrbitControls autoRotate={true} makeDefault  enableZoom={false} minAzimuthAngle={0}/>
+        <OrbitControls maxPolarAngle={11*Math.PI/24}  autoRotate={true} makeDefault  enableZoom={false} />
         <Characters />
       </Canvas>
       <Box sx={{position: "absolute", top: "90vh"}} w="100%">
